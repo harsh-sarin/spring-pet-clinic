4e1cd0de946f4c5d79c9c1257535ee552a37a831
harsh-sarin <h.sarin30@gmail.com>
2020-04-30 20:14:37 -0500

    Added basic POJOs.

---
+++ src/main/java/sarin/spring/petclinic/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package sarin.spring.petclinic.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/sarin/spring/petclinic/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package sarin.spring.petclinic.springpetclinic.model;
+
+public class Vet extends Person {
+}
