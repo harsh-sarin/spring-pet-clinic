1dd9e1f1d5686113074998ca27ed82df118eb10e
harsh-sarin <h.sarin30@gmail.com>
2020-05-02 13:09:20 -0500

    Added controller and view template skeleton.

---
+++ src/main/java/sarin/spring/petclinic/springpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package sarin.spring.petclinic.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String getOwners() {
+        return "owners/index";
+    }
+}
